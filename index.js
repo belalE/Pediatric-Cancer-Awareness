9a056eac1471438b21414aecb8c524eda48b407a
belalE <belalelsiesy@gmail.com>
2021-06-15 16:22:23 -0500

    added ejs and express

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const ejsMate = require('ejs-mate');
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello!')
+    res.render('pages/home')
+})
+app.get('/research', (req, res) => {
+    res.render('pages/research')
+})
+app.get('/types', (req, res) => {
+    res.render('pages/types')
 })
 
 app.listen(3000, () => {
