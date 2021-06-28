e71ea9ec35997d55c2f65a7b2f31c55a2e9bc526
belalE <belalelsiesy@gmail.com>
2021-06-27 23:29:04 -0500

    fixed sticky sidebar
    need to fix resizing
    need to fix scrollspy

--- sidebar.js
+++ sidebar.js
@@ -2,11 +2,4 @@ var scrollSpy = new bootstrap.ScrollSpy(document.querySelector('#content'), {
     target: '#sidebar'
   })
 
-// /* global bootstrap: false */
-// (function () {
-//   'use strict'
-//   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-//   tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-//     new bootstrap.Tooltip(tooltipTriggerEl)
-//   })
-// })()
+console.log(scrollSpy)
